021fd87bb95687457fc16819ae01b337035f3107
kachai <servicios@kachai.io>
2025-11-12 13:52:44 +0000

    fix

--- tests/test.py
+++ tests/test.py
@@ -3,8 +3,8 @@ from main import app
 
 client = TestClient(app)
 
-def test_subtract():
-    r = client.post("/api/subtract", json={"first_number": 10, "second_number": 3})
+def test_sum():
+    r = client.post("/api/sum", json={"first_number": 10, "second_number": 3})
     assert r.status_code == 200
     data = r.json()
-    assert data["sum"] == 7 or data.get("result") == 7
+    assert data["sum"] == 13 or data.get("result") == 13
